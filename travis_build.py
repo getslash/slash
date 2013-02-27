8dc93ba3fcc3de8303286f009428aeb14b76a4c3
Rotem Yaari <vmalloc@gmail.com>
2013-02-27 10:36:35 +0200

    Make tox behave like travis

--- travis_build.py
+++ travis_build.py
@@ -1,13 +1,16 @@
 #! /usr/bin/python
+from __future__ import print_function
 import platform
 import subprocess
 import sys
+import os
 
 def _execute(cmd):
     if 0 != subprocess.call(cmd, shell=True):
         sys.exit(-1)
 
 if __name__ == '__main__':
+    print("Running from", os.path.abspath("."))
     if platform.python_version() < "3.3":
         _execute("pylint --rcfile=.pylintrc setup.py")
         _execute("pylint --rcfile=.pylintrc shakedown")
