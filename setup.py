e97b116deb64fb222ee8b81e1b386da9abdc8861
Rotem Yaari <vmalloc@gmail.com>
2013-06-13 13:40:09 +0300

    Remove Python 3.2 classifier

--- setup.py
+++ setup.py
@@ -26,7 +26,6 @@ setup(name="slash",
       classifiers = [
           "Programming Language :: Python :: 2.6",
           "Programming Language :: Python :: 2.7",
-          "Programming Language :: Python :: 3.2",
           "Programming Language :: Python :: 3.3",
           ],
       description="A Testing Framework",
