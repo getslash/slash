513d5ffb532acd5ddb4875b6c801dff3d4fc4357
Rotem Yaari <vmalloc@gmail.com>
2013-06-25 15:35:55 +0300

    Move interactive shell functionality to the application context

--- slash/app.py
+++ slash/app.py
@@ -1,11 +1,11 @@
 from contextlib import contextmanager
 import sys
-
 from . import hooks as trigger_hook
 from .loader import Loader
 from .session import Session
 from . import site
 from .utils import cli_utils
+from .utils.interactive import start_interactive_shell
 from .utils.reporter import report_context
 
 class Application(object):
@@ -20,12 +20,20 @@ class Application(object):
         self.parser.error(*args, **kwargs)
 
 @contextmanager
-def get_application_context(parser=None, argv=None, args=(), report_stream=sys.stderr):
+def get_application_context(parser=None, argv=None, args=(), report_stream=sys.stderr, enable_interactive=False):
     site.load()
+    args = list(args)
+    if enable_interactive:
+        args.append(
+            cli_utils.Argument("-i", "--interactive", help="Enter an interactive shell",
+                               action="store_true", default=False)
+        )
     with cli_utils.get_cli_environment_context(argv=argv, extra_args=args) as (parser, parsed_args):
         app = Application(parser=parser, args=parsed_args, report_stream=report_stream)
         with app.session:
             with report_context(app.report_stream):
+                if enable_interactive and parsed_args.interactive:
+                    start_interactive_shell()
                 yield app
             trigger_hook.result_summary()
 
--- slash/frontend/slash_run.py
+++ slash/frontend/slash_run.py
@@ -1,7 +1,6 @@
 from ..app import get_application_context
 from ..runner import run_tests
 from ..utils import cli_utils
-from ..utils.interactive import start_interactive_shell
 import logbook
 import sys
 
@@ -10,11 +9,10 @@ _logger = logbook.Logger(__name__)
 def slash_run(args, report_stream=sys.stderr):
     with get_application_context(
             argv=args, args=_get_extra_cli_args(),
+            enable_interactive=True,
             report_stream=report_stream) as app:
         if not app.args.paths and not app.args.interactive:
             app.error("No tests specified")
-        if app.args.interactive:
-            start_interactive_shell()
         for path in app.args.paths:
             run_tests(app.test_loader.iter_path(path))
         if app.session.result.is_success():
@@ -27,6 +25,4 @@ def _get_extra_cli_args():
             "paths", metavar="TEST", nargs="*",
             help="Test name to run. This can be either a file or a test FQDN. "
             "See documentation for details"),
-        cli_utils.Argument("-i", "--interactive", help="Enter an interactive shell before running tests",
-                          action="store_true", default=False),
         ]
