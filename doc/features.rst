b7f504fb84644bde7a98ef2aa1ec289064fed100
Rotem Yaari <vmalloc@gmail.com>
2013-07-31 17:46:49 +0300

    #62: notification configuration is loaded when plugin is activated

--- doc/features.rst
+++ doc/features.rst
@@ -6,9 +6,9 @@ Notifications
 
 Slash provides an optional plugin for sending notifications at end of runs, via ``--with-notifications``. It supports `NMA <http://www.notifymyandroid.com/>`_ and `Prowl <http://www.prowlapp.com/>`_.
 
-To use it, specify either ``notifications.prowl_api_key`` or ``notifications.nma_api_key`` when running. For example::
+To use it, specify either ``plugins.notifications.prowl_api_key`` or ``plugins.notifications.nma_api_key`` when running. For example::
 
-  slash run my_test.py --with-notifications -o notifications.nma_api_key=XXXXXXXXXXXXXXX
+  slash run my_test.py --with-notifications -o plugins.notifications.nma_api_key=XXXXXXXXXXXXXXX
 
 XUnit Export
 ------------
--- slash/plugins/builtin/notifications.py
+++ slash/plugins/builtin/notifications.py
@@ -6,12 +6,25 @@ import requests
 class Plugin(PluginInterface):
     def get_name(self):
         return "notifications"
+
+    def activate(self):
+        config.extend({
+            "plugins": {
+                "notifications" : {
+                    "prowl_api_key" : None,
+                    "nma_api_key" : None,
+            }}})
+
+    def deactivate(self):
+        config["plugins"].pop("notifications")
+
     def session_end(self):
         self._notify("Session Ended", "Session {0} ended".format(session.id))
+
     def _notify(self, title, message):
         for url, api_key in [
-                ("https://prowl.weks.net/publicapi/add", config.root.notifications.prowl_api_key),
-                ("https://www.notifymyandroid.com/publicapi/notify", config.root.notifications.nma_api_key),
+                ("https://prowl.weks.net/publicapi/add", config.root.plugins.notifications.prowl_api_key),
+                ("https://www.notifymyandroid.com/publicapi/notify", config.root.plugins.notifications.nma_api_key),
                 ]:
             if api_key is None:
                 continue
