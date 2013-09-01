5918aca35c7a10b60529934a51650ea6badcaa97
Rotem Yaari <vmalloc@gmail.com>
2013-09-01 11:19:18 +0300

    Coverage via coveralls

--- README.rst
+++ README.rst
@@ -3,9 +3,12 @@ Overview
 
 .. image:: https://secure.travis-ci.org/vmalloc/slash.png?branch=master,dev
 
+.. image:: https://coveralls.io/repos/vmalloc/slash/badge.png?branch=dev
+
 .. image:: https://pypip.in/d/slash/badge.png
 
 .. image:: https://pypip.in/v/slash/badge.png
+
 
 Slash is a testing framework written in Python for testing complex projects. 
 
--- travis_build.py
+++ travis_build.py
@@ -13,6 +13,8 @@ def _execute(cmd):
 if __name__ == '__main__':
     print("Running from", os.path.abspath("."))
     deps = [
+        "coverage",
+        "coveralls",
         "nose",
         "pyforge",
         "lxml", # for XSD validations
@@ -29,4 +31,4 @@ if __name__ == '__main__':
     if not _PYPY:
         _execute("pylint --rcfile=.pylintrc setup.py")
         _execute("pylint --rcfile=.pylintrc slash")
-    _execute("nosetests -w tests")
+    _execute("nosetests  --with-coverage --cover-package slash -w tests")
