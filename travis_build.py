1189421d07f9b8665ae8c67bf6794bc0f0759254
Rotem Yaari <vmalloc@gmail.com>
2013-09-11 15:51:59 +0300

    Coverage fixes

--- travis_build.py
+++ travis_build.py
@@ -31,4 +31,4 @@ if __name__ == '__main__':
     if not _PYPY:
         _execute("pylint --rcfile=.pylintrc setup.py")
         _execute("pylint --rcfile=.pylintrc slash")
-    _execute("nosetests  --with-coverage --cover-package slash -w tests")
+    _execute("coverage run $(which nosetests) -w tests")
