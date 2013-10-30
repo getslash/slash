72e73f0d8a127fe7d3f4cf332897c8e1cc705198
Rotem Yaari <vmalloc@gmail.com>
2013-10-30 18:25:28 +0200

    Update confetti requirement

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(_in_same_dir("slash", "__version__.py")) as version_file:
 
 install_requires = [
     "colorama",
-    "confetti>=2.0.0",
+    "confetti>=2.1.0",
     "emport==1.0.0",
     "logbook>=0.4.2",
     "requests>=1.1.0",
