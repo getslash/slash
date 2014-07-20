51b7eedb6203487dd05dcf0e912e92c6c122ae67
Rotem Yaari <vmalloc@gmail.com>
2014-07-20 13:08:43 +0300

    Cosmetics

--- tests/test_readme_doctest.py
+++ tests/test_readme_doctest.py
@@ -1,10 +1,8 @@
-from unittest import TestCase
 import os
 import doctest
 
-class ReadMeDocTest(TestCase):
-    def test_readme_doctests(self):
-        readme_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "README.md"))
-        self.assertTrue(os.path.exists(readme_path))
-        result = doctest.testfile(readme_path, module_relative=False)
-        self.assertEquals(result.failed, 0, "%s tests failed!" % result.failed)
+def test_readme_doctests():
+    readme_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "README.md"))
+    assert os.path.exists(readme_path)
+    result = doctest.testfile(readme_path, module_relative=False)
+    assert result.failed == 0, ('%s tests failed!' % result.failed)
