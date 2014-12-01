bb441552844195d66022069fdd3df809fc3e02c4
Rotem Yaari <vmalloc@gmail.com>
2014-12-01 15:20:34 +0200

    Don't enter debugger on SystemExit exceptions

--- slash/utils/debug.py
+++ slash/utils/debug.py
@@ -52,6 +52,8 @@ def debug_if_needed(exc_info=None):
         exc_info = sys.exc_info()
     if exc_info[0] is SkipTest and not config.root.debug.debug_skips:
         return
+    if isinstance(exc_info[1], (SystemExit,)):
+        return
 
     launch_debugger(exc_info)
 
