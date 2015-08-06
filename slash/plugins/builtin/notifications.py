46484bc9fa863496b1d97f7fb51ac6dba800e21f
Roey Darwish Dror <roeyd@infinidat.com>
2015-08-06 11:50:56 +0300

    Improve notification plugin

--- slash/plugins/builtin/notifications.py
+++ slash/plugins/builtin/notifications.py
@@ -2,6 +2,7 @@ from ..interface import PluginInterface
 from ...conf import config
 from ...ctx import session
 from functools import partial
+import socket
 import requests
 
 
@@ -27,26 +28,25 @@ class Plugin(PluginInterface):
     def get_name(self):
         return "notifications"
 
-    def activate(self):
-        config.extend({
-            "plugins": {
-                "notifications" : {
-                    "prowl_api_key" : None,
-                    "nma_api_key" : None,
-                    "pushbullet_api_key": None,
-            }}})
-
-    def deactivate(self):
-        config["plugins"].pop("notifications")
+    def get_config(self):
+        return {
+            "prowl_api_key" : None,
+            "nma_api_key" : None,
+            "pushbullet_api_key": None,
+        }
 
     def session_end(self):
-        self._notify("Session Ended", "Session {0} ended".format(session.id))
+        result = "successfully" if session.results.is_success() else "unsuccessfully"
+        hostname = socket.gethostname().split(".")[0]
+        body = "{0}\n\n{1}".format(session.results, session.id)
+
+        self._notify("Slash session in {0} ended {1}".format(hostname, result), body)
 
     def _notify(self, title, message):
         for func, api_key in [
-                (partial(_post_notification, "https://prowl.weks.net/publicapi/add"), config.root.plugins.notifications.prowl_api_key),
-                (partial(_post_notification, "https://www.notifymyandroid.com/publicapi/notify"), config.root.plugins.notifications.nma_api_key),
-                (_pushbullet_notification, config.root.plugins.notifications.pushbullet_api_key),
+                (partial(_post_notification, "https://prowl.weks.net/publicapi/add"), config.root.plugin_config.notifications.prowl_api_key),
+                (partial(_post_notification, "https://www.notifymyandroid.com/publicapi/notify"), config.root.plugin_config.notifications.nma_api_key),
+                (_pushbullet_notification, config.root.plugin_config.notifications.pushbullet_api_key),
                 ]:
             if api_key is None:
                 continue
