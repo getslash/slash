3950568a56516e7899d0f886bef013ccb597573f
Rotem Yaari <vmalloc@gmail.com>
2015-08-11 16:20:08 +0300

    Add debug logs for cleanup code

--- slash/core/cleanup_manager.py
+++ slash/core/cleanup_manager.py
@@ -68,13 +68,15 @@ class CleanupManager(object):
         return self._scope_stack[-1]
 
     def push_scope(self, scope_name):
+        _logger.debug('CleanupManager: pushing scope {0!r}', scope_name)
         scope = _Scope(scope_name)
         self._scope_stack.append(scope)
         self._scopes_by_name.setdefault(scope_name, []).append(scope)
 
     def pop_scope(self, scope_name, in_failure=None, in_interruption=None):
+        _logger.debug('CleanupManager: popping scope {0!r} (failure: {1}, interrupt: {2})', scope_name, in_failure, in_interruption)
         scope = self._scope_stack[-1]
-        assert scope.name == scope_name # pylint: disable=no-member
+        assert scope.name == scope_name, 'Attempted to pop scope {0!r}, but current scope is {1!r}'.format(scope_name, scope.name)
         self._scope_stack.pop()
         self._scopes_by_name[scope_name].pop()
 
