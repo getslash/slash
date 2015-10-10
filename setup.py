89156bc63f6e60181d07bbd048b52d78022fd2d7
Rotem Yaari <vmalloc@gmail.com>
2015-10-10 10:41:30 +0300

    Fix 2.6 support

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 import functools
 import os
-import platform
+import sys
 
-_PYTHON_VERSION = platform.python_version()
+_PYTHON_VERSION = sys.version_info
 _in_same_dir = functools.partial(os.path.join, os.path.dirname(__file__))
 
 with open(_in_same_dir("slash", "__version__.py")) as version_file:
@@ -16,7 +16,6 @@ install_requires = [
     "dessert",
     "emport>=1.0.0",
     "gossip>=1.0.0",
-    "IPython",
     "Logbook>=0.11.2",
     "requests>=1.1.0",
     "raven",
@@ -25,11 +24,18 @@ install_requires = [
     # DO NOT ADD pyforge, lxml or any other package only required for testing
 ]
 
-if _PYTHON_VERSION < "2.7":
+if _PYTHON_VERSION < (2, 7):
     install_requires.append("argparse")
     install_requires.append("ordereddict")
 
-if _PYTHON_VERSION < "3.3":
+# Ipython dep
+if _PYTHON_VERSION < (2, 7):
+    install_requires.insert(0, "ptyprocess==0.4") # 0.5 dropped 2.6 support
+    install_requires.append('IPython==1.2.1')
+else:
+    install_requires.append('IPython')
+
+if _PYTHON_VERSION < (3, 3):
     install_requires.append("contextlib2")
 
 setup(name="slash",
