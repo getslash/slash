7345006b5807542e088e40f4f3c63225a314a61b
Rotem Yaari <vmalloc@gmail.com>
2015-11-25 09:25:47 +0200

    fix handling special parameter names when deducing function names (#389)

--- tests/utils/suite_writer/parameter.py
+++ tests/utils/suite_writer/parameter.py
@@ -5,10 +5,12 @@ from .element import Element
 
 class Parameter(Element):
 
-    def __init__(self, suite, num_values=3):
+    def __init__(self, suite, num_values=3, values=None):
         super(Parameter, self).__init__(suite)
         self.name = 'param_{0}'.format(self.id)
-        self.values = [str(uuid4()) for i in range(num_values)]
+        if values is None:
+            values = [str(uuid4()) for _ in range(num_values)]
+        self.values = values
 
     def write_decorator(self, code_formatter):
         code_formatter.writeln('@slash.parametrize({0!r}, {1!r})'.format(
