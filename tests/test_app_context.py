5211b40ec3de0a7c6c1137fa1658668ff21fc108
Rotem Yaari <vmalloc@gmail.com>
2016-05-06 16:04:37 +0300

    Fix travis tests

--- tests/test_app_context.py
+++ tests/test_app_context.py
@@ -8,5 +8,5 @@ from slash.reporting.null_reporter import NullReporter
 
 @pytest.mark.parametrize('use_reporter', [True, False])
 def test_app_context_reporter(use_reporter):
-    with get_application_context(report=use_reporter) as app:
+    with get_application_context(report=use_reporter, argv=[]) as app:
         assert isinstance(app.session.reporter, ConsoleReporter if use_reporter else NullReporter)
