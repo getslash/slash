4cc39cc05579d3eb6992849fa71e26a868ab655c
Rotem Yaari <vmalloc@gmail.com>
2016-05-28 21:12:29 +0300

    Cosmetics

--- tests/test_warnings.py
+++ tests/test_warnings.py
@@ -1,3 +1,4 @@
+#pylint: disable=unused-variable
 import pytest
 import slash
 
@@ -8,15 +9,19 @@ from .utils import run_tests_assert_success, without_pyc
 def test_str_repr(warning, reprify):
     assert 'this is a warning' in reprify(warning)
 
+
 def test_location(warning):
     assert warning.details['filename'] == without_pyc(__file__)
+
 
 def test_to_dict(warning):
     assert isinstance(warning.to_dict(), dict)
 
+
 def test_warning_added_hook(suite, suite_test):
 
     captured = []
+
     @slash.hooks.register
     def warning_added(warning):
         captured.append(warning)
@@ -24,16 +29,18 @@ def test_warning_added_hook(suite, suite_test):
     suite_test.append_line('slash.logger.warning("message here")')
     suite.run()
     assert captured
-    [w] = captured
+    [w] = captured # pylint: disable=unbalanced-tuple-unpacking
     assert w.message == 'message here'
     assert isinstance(w.lineno, int)
     assert isinstance(w.filename, str)
     assert w.lineno
     assert w.filename
 
+
 @pytest.fixture
 def warning():
     class SampleTest(slash.Test):
+
         def test(self):
             slash.logger.warning("this is a warning. Param: {0}", 1)
 
