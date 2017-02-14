e391d2b244c901f0eb4e1def543daa55ee8584e9
Rotem Yaari <vmalloc@gmail.com>
2017-02-14 21:53:03 +0200

    Fix slash_run test

--- tests/test_slash_run.py
+++ tests/test_slash_run.py
@@ -16,7 +16,7 @@ from .utils import no_op, NullFile, TestCase
 def test_slash_run_fails_fast_for_missing_files():
     result = slash_run.slash_run(
         ["/non/existing/path"], report_stream=NullFile())
-    assert result != 0, "slash run unexpectedly succeeded for a missing path"
+    assert result.exit_code != 0, "slash run unexpectedly succeeded for a missing path"
 
 
 ################################################################################
