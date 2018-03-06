ee9762fa15c3f62b0adfe01e06accd74a74f338c
Rotem Yaari <vmalloc@gmail.com>
2018-03-06 14:50:21 +0200

    Fix hook documentation generation

--- doc/hook_list_doc.py
+++ doc/hook_list_doc.py
@@ -12,7 +12,7 @@ class HookListDoc(Directive):
         returned = []
         all_hooks = []
         for hook in sorted(gossip.get_group("slash").get_hooks(), key=lambda hook:hook.name):
-            section = nodes.section(ids=[hook.name])
+            section = nodes.section(ids=[hook.name], names=['hooks.{}'.format(hook.name)])
             returned.append(section)
             title = "slash.hooks.{0}".format(hook.name)
             args = hook.get_argument_names()
