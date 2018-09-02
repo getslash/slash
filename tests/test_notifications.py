f626c0b8895339848be13fe11c3c7b6a52a235f5
Rotem Yaari <vmalloc@gmail.com>
2018-09-02 10:13:26 +0300

    Fix notification formatting (closes #830)

--- tests/test_notifications.py
+++ tests/test_notifications.py
@@ -1,12 +1,22 @@
+import sys
 import slash
 
 from slash.plugins import manager as plugin_manager
 
 
 def test_notification_formatting():
+    messages = []
+
+    @slash.hooks.prepare_notification.register # pylint: disable=no-member
+    def check_notification(message): # pylint: disable=unused-variable
+        messages.append(message)
+
     plugin = plugin_manager.get_plugin('notifications')
+
     with slash.Session():
-        msg = plugin._get_message('{bla}', True) # pylint: disable=protected-access
-    assert msg.get_title()
-    assert '{bla}' in msg.get_short_message()
-    assert msg.get_html_message()
+        try:
+            raise Exception('Some message containing {curly_braces}')
+        except Exception: # pylint: disable=broad-except
+            plugin.entering_debugger(sys.exc_info())
+    assert len(messages) == 1
+    assert '{curly_braces}' in messages[0].get_short_message()
