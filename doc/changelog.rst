1f9c4fb8b8ab21ed7a693b3fc0242022fc73f64b
Ayala Shachar <shachar.ayala@gmail.com>
2018-10-08 18:49:34 +0300

    Fix handling exceptions during Application.__exit__() (closes #879)

--- doc/changelog.rst
+++ doc/changelog.rst
@@ -1,6 +1,7 @@
 Changelog
 =========
 
+* :bug:`879 major` Fix handling exceptions during ``entering_debugger`` hook triggered by ``Application.__exit__()``
 * :release:`1.7.1 <7-10-2018>`
 * :bug:`876 major`  Support disabling convertion of log error to slash-error by extra's ``capture=False``
 * :release:`1.7.0 <7-10-2018>`
--- tests/test_app_context.py
+++ tests/test_app_context.py
@@ -23,3 +23,16 @@ def test_custom_reporter():
     app.set_reporter(dummy_reporter)
     with app:
         assert app.session.reporter is dummy_reporter
+
+
+def test_exception_during_app_exit_debugger(checkpoint, config_override):
+    @hooks.entering_debugger.register  # pylint: disable=no-member
+    def callback(*_, **__):  # pylint: disable=unused-variable
+        checkpoint()
+        raise Exception('Entering debugger error')
+    config_override('debug.enabled', True)
+    with Application() as app:
+        raise Exception('Some Exception')
+    assert checkpoint
+    assert app.exit_code != 0
+    assert checkpoint.called_count == 1
