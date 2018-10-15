c6a044cb3a84b2202605a7f794a6065f6865db00
Maor Marcus <mmarcus@infinidat.com>
2018-10-15 11:09:10 +0300

    INFRADEV-11243: Support --unstarted-only and --failed-only flags for slash resume

--- slash/__version__.py
+++ slash/__version__.py
@@ -1,3 +1,11 @@
 import pkg_resources
 
 __version__ = pkg_resources.get_distribution('slash').version
+
+def get_backslash_client_version():
+    try:
+        return pkg_resources.get_distribution('backslash').version
+    except pkg_resources.DistributionNotFound:
+        return None
+
+__backslash_version__ = get_backslash_client_version()
